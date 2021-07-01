eebc515c34d185fd50b54fa4d431eec66980c430
Byron Keet <keetbis@gmail.com>
2021-07-01 15:57:29 +0200

    Add Screens

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const FavoritesScreen = () => {
+    return (
+        <View>
+            <Text>The Meal Detail Screen!</Text>
+        </View>
+    )
+};
+
+export default FavoritesScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
