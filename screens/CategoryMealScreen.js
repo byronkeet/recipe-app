c8c8405032615ffdb2e3a3e58a4d06a199666a14
Byron Keet <keetbis@gmail.com>
2021-07-06 15:49:28 +0200

    Add mealItem and header icon

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,21 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import {CATEGORIES} from '../data/dummy-data';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = (props) => {
     const catId = props.navigation.getParam('categoryId');
-    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+    const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
+
+    const renderMealItem = itemData => {
+        const {title, imageUrl, duration, complexity, affordability, id} = itemData.item;
+        return (
+            <MealItem
+                title={title}
+                image={imageUrl}
+                duration={duration}
+                complexity={complexity}
+                affordability={affordability}
+                onSelectMeal={() => {
+                    props.navigation.navigate({
+                        routeName: 'MealDetail',
+                        params: {
+                            mealId: id
+                        }
+                    })
+                }}
+            />
+        )
+    }
+
     return (
         <View style={styles.screen}>
-            <Text>The Category Meal Screen!</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button
-                title='Meal Details'
-                onPress={() => props.navigation.navigate({ routeName: 'MealDetail' })} 
-            />
-            <Button
-                title='Go Back'
-                onPress={() => props.navigation.pop()} 
+            <FlatList
+                data={displayedMeals}
+                renderItem={renderMealItem}
+                style={{ width: '100%'}}
             />
         </View>
     )
@@ -36,6 +54,7 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        padding: 15
     }
 });
